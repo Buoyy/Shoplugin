dbdf03abd718d517164c7e8a32a98762976945ac
Buoyy <81320656+Buoyy@users.noreply.github.com>
2025-02-21 20:09:54 +0500

    Updated instancing

--- src/main/java/com/github/buoyy/shoplugin/Shoplugin.java
+++ src/main/java/com/github/buoyy/shoplugin/Shoplugin.java
@@ -10,13 +10,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.Objects;
 
 public class Shoplugin extends JavaPlugin {
-    private static final Messenger messenger = new Messenger();
-    private static final GUIManager guiManager = new GUIManager();
-    private static final YAML general = new YAML("general");
+    private static Messenger messenger;
+    private static GUIManager guiManager;
+    private static YAML general;
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new GUIListener(guiManager), this);
-        Objects.requireNonNull(getCommand("shop")).setExecutor(new BaseShopCommand());
+instanceObjects(); getServer().getPluginManager().registerEvents(new GUIListener(guiManager), this);      Objects.requireNonNull(getCommand("shop")).setExecutor(new BaseShopCommand());
+    }
+    private void instanceObjects() {
+        messenger = new Messenger();
+        guiManager = new GUIManager();
+        general = new YAML("general");
     }
     public static GUIManager getGuiManager() {
         return guiManager;
